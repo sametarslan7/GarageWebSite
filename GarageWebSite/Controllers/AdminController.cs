0813bd8ad4551a1f1496ad119a7d12082e88cb2e
Samet Arslan <sametarslan1129@gmail.com>
2024-10-31 10:27:29 +0300

    updated admin panel - cars table

--- GarageWebSite/Controllers/AdminController.cs
+++ GarageWebSite/Controllers/AdminController.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity;
+
 
 namespace GarageWebSite.Controllers
 {
@@ -15,26 +17,60 @@ namespace GarageWebSite.Controllers
         {
             return View();
         }
+        
+        // ADMIN
         public ActionResult AdminList()
         {
             var value = c.Admins.ToList();
             return View(value);
         }
+
+        //CARS
+        public ActionResult CarList(string brand, string year, string fuel)
+        {
+            // Cars tablosuna FuelType, Photos ve Brand dahil edilerek gerekli verileri tek seferde getiriyoruz.
+            var carsQuery = c.Cars
+                .Include(c => c.FuelType)
+                .Include(c => c.Photos)
+                .Include(c => c.Brand)
+                .AsQueryable(); // AsQueryable ile sorgu oluşturuyoruz.
+
+            // Sorguyu çalıştırıyoruz
+            var cars = carsQuery.ToList();
+
+            // ViewModel oluşturma
+            var carViewModels = cars.Select(car => new CarViewModel
+            {
+                Car = car,
+                FuelTypeName = car.FuelType.FuelName, // Yakıt türünü doğrudan alıyoruz
+                Photos = car.Photos.ToList(), // Araca ait tüm fotoğrafları alıyoruz
+                BrandName = car.Brand.BrandName // Araca ait markayı alıyoruz
+            }).ToList();
+            return View(carViewModels);
+        }
+
+        //BRAND
         public ActionResult BrandList()
         {
             var value = c.Brands.ToList();
             return View(value);
         }
+
+        //FUELTYPE
         public ActionResult FuelTypeList()
         {
             var value = c.FuelTypes.ToList();
             return View(value);
         }
+
+        //SERVICE
         public ActionResult ServiceList()
         {
             var value = c.Services.ToList();
             return View(value);
         }
+
+        //CONTACT
         public ActionResult ContactList()
         {
             var value = c.Contacts.ToList();
